1d65c3cbba1a309c17574ce4b77d54e6eb20c902
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-11-14 00:32:08 +0100

    Cleaning and adding one funny alias

--- index.py
+++ index.py
@@ -2,27 +2,23 @@ import requests
 
 from discord import app_commands, Intents, Client, Interaction
 
-
-def check_me(token_test: str) -> dict:
-    r = requests.get("https://discord.com/api/v10/users/@me", headers={
-        "Authorization": f"Bot {token_test}"
-    })
-
-    return r.json()
-
-
 print("\n".join([
     "Hey, welcome to the active developer badge bot.",
     "Please enter your bot's token below to continue.",
     "",
-    "Don't close this application after entering the token. You may close it after the bot has been invited and the command has been ran."
+    "Don't close this application after entering the token. "
+    "You may close it after the bot has been invited and the command has been ran."
 ]))
 
 
 while True:
     token = input("> ")
-    data = check_me(token)
 
+    r = requests.get("https://discord.com/api/v10/users/@me", headers={
+        "Authorization": f"Bot {token}"
+    })
+
+    data = r.json()
     if data.get("id", None):
         break
 
@@ -51,22 +47,35 @@ async def on_ready():
     ]))
 
 
-@client.tree.command()
-async def hello(interaction: Interaction):
-    """ Says hello or something """
+async def _init_command_response(interaction: Interaction):
     print(f"> {interaction.user} used the command.")
     await interaction.response.send_message("\n".join([
         f"Hi **{interaction.user}**, thank you for saying hello to me.",
         "",
         "__**Where's my badge?**__",
-        "Eligibility for the badge is checked by Discord in intervals, at this moment in time, 24 hours is the recommended time to wait before trying.",
+        "Eligibility for the badge is checked by Discord in intervals, "
+        "at this moment in time, 24 hours is the recommended time to wait before trying.",
         "",
         "__**It's been 24 hours, now how do I get the badge?**__",
-        "If it's already been 24 hours, you can head to https://discord.com/developers/active-developer and fill out the 'form' there.",
+        "If it's already been 24 hours, you can head to "
+        "https://discord.com/developers/active-developer and fill out the 'form' there.",
         "",
         "__**Active Developer Badge Updates**__",
-        "Updates regarding the Active Developer badge can be found in the Discord Developers server -> discord.gg/discord-developers - in the #active-developer-badge channel.",
+        "Updates regarding the Active Developer badge can be found in the "
+        "Discord Developers server -> discord.gg/discord-developers - in the #active-developer-badge channel.",
     ]))
+
+
+@client.tree.command()
+async def hello(interaction: Interaction):
+    """ Says hello or something """
+    await _init_command_response(interaction)
+
+
+@client.tree.command()
+async def givemebadge(interaction: Interaction):
+    """ Says hello or something, but with a different name """
+    await _init_command_response(interaction)
 
 
 client.run(token)
