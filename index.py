dd25733bca2c947e36e2c56a9bc2d0751e9e2ddd
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-12-15 13:00:07 +0100

    Adding these

--- index.py
+++ index.py
@@ -1,20 +1,38 @@
 import requests
+import json
+import inspect
 
 from discord import app_commands, Intents, Client, Interaction
 
 # This will print the information in console
-print("\n".join([
-    "Hey, welcome to the active developer badge bot.",
-    "Please enter your bot's token below to continue.",
-    "",
-    "Don't close this application after entering the token. "
-    "You may close it after the bot has been invited and the command has been ran."
-]))
+# inspect.cleandoc() is used to remove the indentation from the message
+# while using triple quotes so that it looks nice in the code
+# Normally some developers don't use it and move the text all the way back to the left
+# However I personally prefer clean code
+print(inspect.cleandoc("""
+    Hey, welcome to the active developer badge bot.
+    Please enter your bot's token below to continue.
+
+    Don't close this application after entering the token
+    You may close it after the bot has been invited and the command has been ran
+"""))
+
+
+try:
+    with open("./config.json", "r", encoding="utf8") as f:
+        config = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    # You can in theory also do "except Exception", but it is not recommended
+    config = {}
 
 
 while True:
     # While loop starts and used Python.input() to get the token
-    token = input("> ")
+    token = config.get("token", None)
+    if token:
+        print("\n--- Detected token in config.json saved from previous run, using that. ---\n")
+    else:
+        token = input("> ")
 
     # Then validates if the token you provided was correct or not
     r = requests.get("https://discord.com/api/v10/users/@me", headers={
@@ -28,7 +46,18 @@ while True:
 
     # If the token is incorrect, it will print the error message
     # and ask you to enter the token again (while Loop)
-    print("\nSeems like you entered an invalid token. Try again.")
+    print("\nSeems like you entered an invalid token. Try again by entering the correct token.")
+    # Resets the config so that it doesn't use the previous token again
+    config = {}
+
+# This is used to save the token for the next time you run the bot
+with open("./config.json", "w", encoding="utf8") as f:
+    # Making sure that 'token' key exists in the config.json file
+    config["token"] = token
+    # This dumps our working setting to the config.json file
+    # Indent is used to make the file look nice and clean
+    # If you don't want to indent, you can remove the indent=2 from code
+    json.dump(config, f, indent=2)
 
 
 class FunnyBadge(Client):
@@ -53,54 +82,36 @@ async def on_ready():
         It also prints out the bot's invite URL that automatically uses your
         Client ID to make sure you invite the correct bot with correct scopes.
     """
-    print("\n".join([
-        f"Logged in as {client.user} (ID: {client.user.id})",
-        "",
-        f"Use this URL to invite {client.user} to your server:",
-        f"https://discord.com/api/oauth2/authorize?client_id={client.user.id}&scope=applications.commands%20bot"
-    ]))
+    print(inspect.cleandoc(f"""
+        Logged in as {client.user} (ID: {client.user.id})
 
-
-async def _init_command_response(interaction: Interaction) -> None:
-    """ This is called when the command is ran
-        The reason the command is outside of the command function
-        is because there are two ways to run the command and slash commands
-        do not natevily support aliases, so we have to fake it.
-    """
-
-    # Responds in the console that the command has been ran
-    print(f"> {interaction.user} used the command.")
-
-    # Then responds in the channel with this message
-    await interaction.response.send_message("\n".join([
-        f"Hi **{interaction.user}**, thank you for saying hello to me.",
-        "",
-        "__**Where's my badge?**__",
-        "Eligibility for the badge is checked by Discord in intervals, "
-        "at this moment in time, 24 hours is the recommended time to wait before trying.",
-        "",
-        "__**It's been 24 hours, now how do I get the badge?**__",
-        "If it's already been 24 hours, you can head to "
-        "https://discord.com/developers/active-developer and fill out the 'form' there.",
-        "",
-        "__**Active Developer Badge Updates**__",
-        "Updates regarding the Active Developer badge can be found in the "
-        "Discord Developers server -> discord.gg/discord-developers - in the #active-dev-badge channel.",
-    ]))
+        Use this URL to invite {client.user} to your server:
+        https://discord.com/api/oauth2/authorize?client_id={client.user.id}&scope=applications.commands%20bot
+    """))
 
 
 @client.tree.command()
 async def hello(interaction: Interaction):
     """ Says hello or something """
-    # Calls the function "_init_command_response" to respond to the command
-    await _init_command_response(interaction)
+    # Responds in the console that the command has been ran
+    print(f"> {interaction.user} used the command.")
 
+    # Then responds in the channel with this message
+    await interaction.response.send_message(inspect.cleandoc(f"""
+        Hi **{interaction.user}**, thank you for saying hello to me.
 
-@client.tree.command()
-async def givemebadge(interaction: Interaction):
-    """ Says hello or something, but with a different name """
-    # Calls the function "_init_command_response" to respond to the command
-    await _init_command_response(interaction)
+        __**Where's my badge?**__
+        Eligibility for the badge is checked by Discord in intervals,
+        at this moment in time, 24 hours is the recommended time to wait before trying.
+
+        __**It's been 24 hours, now how do I get the badge?**__
+        If it's already been 24 hours, you can head to
+        https://discord.com/developers/active-developer and fill out the 'form' there.
+
+        __**Active Developer Badge Updates**__
+        Updates regarding the Active Developer badge can be found in the
+        Discord Developers server -> https://discord.gg/discord-developers - in the #active-dev-badge channel.
+    """))
 
 
 # Runs the bot with the token you provided
